91303315fdadfe72dd80dea0efef7f405ad77faa
Mohit Jain <mohitjain711@outlook.com>
2015-09-01 03:20:31 +0000

    MyStories -- Work in progress

---
+++ mojoportal/Web/Controls/SMT_MyStories.ascx
@@ -0,0 +1,14 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SMT_MyStories.ascx.cs" Inherits="mojoPortal.Web.Controls._SMT_MyStories" %>
+<portal:mojoLabel ID="mojoLabel1" runat="server" Text ="My Stories"></portal:mojoLabel>
+
+
+
+<asp:Repeater ID="myStoriesRepeater" runat="server">
+    <HeaderTemplate>
+    </HeaderTemplate>
+</asp:Repeater>
+<%--<asp:SqlDataSource ID="myStoriesDataSource" runat="server"></asp:SqlDataSource>--%>
+
+
+
+
---
+++ mojoportal/Web/Controls/SMT_MyStories.ascx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace mojoPortal.Web.Controls
+{
+    public partial class SMT_MyStories : mojoPortal.Web.SiteModuleControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connection = System.Configuration.ConfigurationManager.AppSettings["MSSQLConnectionString"];
+            SqlConnection conn = new SqlConnection(connection);
+        }
+
+        protected void FetchStoriesList(string emailId, SqlConnection conn)
+        {
+            SqlCommand cmd = new SqlCommand(Utilities.myStoriesCmd, conn);
+            conn.Open();
+            cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+        }
+    }
+}
