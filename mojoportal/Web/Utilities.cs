d1120a7db07a749b5a7075e9aeb852a2ad4c20f6
Mohit Jain <mohitjain711@outlook.com>
2015-09-01 03:21:27 +0000

    Utilities File -- For miscellaneous utilities and strings.

---
+++ mojoportal/Web/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mojoPortal.Web
+{
+    public class Utilities
+    {
+        public static string myStoriesCmd = "SELECT * FROM mp_Blogs";
+    }
+}
